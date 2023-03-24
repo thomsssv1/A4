76cbd4573aee3fc2219ab64fc2daf6e9c1bc843a
thomsssv1 <thomasni.2001@gmail.com>
2023-03-25 00:34:54 +0100

    finalisation du dashboard mtn faut mettre sur un serveur

--- dashboard.py
+++ dashboard.py
@@ -1,72 +1,103 @@
-import os
-import pandas as pd
-import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_table
+import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
+import os
+import time
 
 def load_data():
-    if not os.path.exists("data.csv"):
-        with open("data.csv", "w") as f:
-            f.write("timestamp,rate\n")
-
     data = pd.read_csv("data.csv")
-    if "timestamp" not in data.columns or "rate" not in data.columns:
-        raise Exception("Le fichier data.csv ne contient pas les colonnes 'timestamp' et 'rate' requises.")
-
     data["timestamp"] = pd.to_datetime(data["timestamp"])
     return data
 
-def calculate_daily_metrics(data):
-    today_data = data[data["timestamp"].dt.date == pd.Timestamp.now().date()]
-    open_price = today_data.iloc[0]["rate"]
-    close_price = today_data.iloc[-1]["rate"]
-    daily_change = close_price - open_price
-    daily_volatility = today_data["rate"].std()
-
-    return {
-        "open_price": open_price,
-        "close_price": close_price,
-        "daily_change": daily_change,
-        "daily_volatility": daily_volatility
-    }
+def daily_report(data):
+    data['date'] = data['timestamp'].dt.date
+    daily_data = data.groupby('date').agg(
+        open_rate=('rate', 'first'),
+        close_rate=('rate', 'last'),
+        high_rate=('rate', 'max'),
+        low_rate=('rate', 'min')
+    )
+    daily_data['percentage_change'] = (daily_data['close_rate'] - daily_data['open_rate']) / daily_data['open_rate'] * 100
+    return daily_data
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    dcc.Interval(id='interval-component', interval=5*60*1000, n_intervals=0),
-    html.H1("Taux de change EUR/USD"),
-    dcc.Graph(id='time-series-graph'),
-    html.Div(id='current-rate'),
-    html.Div(id='daily-report')
-])
+data = load_data()
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Taux de change EUR/USD"),
+        dcc.DatePickerRange(
+            id='date-picker',
+            min_date_allowed=data['timestamp'].min().date(),
+            max_date_allowed=data['timestamp'].max().date(),
+            start_date=data['timestamp'].min().date(),
+            end_date=data['timestamp'].max().date()
+        ),
+        dcc.Graph(id="live-update-graph"),
+        dcc.Interval(
+            id="interval-component",
+            interval=60 * 1000,  # Mise à jour toutes les minutes
+            n_intervals=0,
+        ),
+        html.H2(children="Rapport quotidien"),
+        dash_table.DataTable(
+            id="daily-report-table",
+            columns=[
+                {"name": "Date", "id": "date"},
+                {"name": "Ouverture", "id": "open_rate"},
+                {"name": "Clôture", "id": "close_rate"},
+                {"name": "Haut", "id": "high_rate"},
+                {"name": "Bas", "id": "low_rate"},
+                {"name": "Variation en %", "id": "percentage_change"},
+            ],
+            style_cell={"textAlign": "center"},
+            style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"},
+        ),
+    ]
+)
 
 @app.callback(
-    Output('time-series-graph', 'figure'),
-    Output('current-rate', 'children'),
-    Output('daily-report', 'children'),
-    Input('interval-component', 'n_intervals')
+    Output("live-update-graph", "figure"),
+    Input("interval-component", "n_intervals"),
+    Input('date-picker', 'start_date'),
+    Input('date-picker', 'end_date')
 )
-def update_dashboard(_):
+
+def update_dashboard(n, start_date, end_date):
     data = load_data()
-    current_rate = data.iloc[-1]["rate"]
-    current_rate_text = f"Taux actuel : {current_rate}"
+    data = data[(data['timestamp'].dt.date >= pd.to_datetime(start_date)) & (data['timestamp'].dt.date <= pd.to_datetime(end_date))]
+    
+    trace = go.Scatter(x=data["timestamp"], y=data["rate"], mode="lines+markers", name="Taux de change")
 
-    fig = px.line(data, x="timestamp", y="rate", title="Taux de change EUR/USD au cours du temps")
-    fig.update_xaxes(title="Date et heure")
-    fig.update_yaxes(title="Taux de change")
+    data["moving_average"] = data["rate"].rolling(window=5).mean()
+    moving_average_trace = go.Scatter(x=data["timestamp"], y=data["moving_average"], mode="lines", name="Moyenne mobile")
 
-    daily_metrics = calculate_daily_metrics(data)
-    daily_report = [
-        html.H3("Rapport quotidien"),
-        html.P(f"Prix d'ouverture : {daily_metrics['open_price']}"),
-        html.P(f"Prix de clôture : {daily_metrics['close_price']}"),
-        html.P(f"Changement quotidien : {daily_metrics['daily_change']}"),
-        html.P(f"Volatilité quotidienne : {daily_metrics['daily_volatility']}")
-    ]
+    data["volatility"] = data["rate"].rolling(window=5).std()
+    volatility_trace_upper = go.Scatter(x=data["timestamp"], y=data["moving_average"] + data["volatility"], mode="lines", name="Volatilité supérieure", line={"dash": "dash"})
+    volatility_trace_lower = go.Scatter(x=data["timestamp"], y=data["moving_average"] - data["volatility"], mode="lines", name="Volatilité inférieure", line={"dash": "dash"})
 
-    return fig, current_rate_text, daily_report
+    return {
+        "data": [trace, moving_average_trace, volatility_trace_upper, volatility_trace_lower],
+        "layout": go.Layout(
+            xaxis={"title": "Date et heure"},
+            yaxis={"title": "Taux de change"},
+            showlegend=True,
+            margin=dict(l=40, r=0, t=40, b=30),
+        ),
+    }
+@app.callback(
+    Output("daily-report-table", "data"),
+    Input("interval-component", "n_intervals"),
+)
+def update_daily_report(n):
+    data = load_data()
+    daily_data = daily_report(data)
+    return daily_data.reset_index().to_dict("records")
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- scrape_eur_usd.sh
+++ scrape_eur_usd.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-URL="https://www.x-rates.com/calculator/?from=EUR&to=USD"
-OUTPUT_FILE="data.csv"
+URL="https://www.x-rates.com/calculator/?from=EUR&to=USD&amount=1"
+OUTPUT_DIR="/home/parallels/A4"
+OUTPUT_FILE="$OUTPUT_DIR/data.csv"
+
+# Créez le dossier de sortie si nécessaire
+mkdir -p "$OUTPUT_DIR"
+
+# Créez un nouveau fichier data.csv avec les en-têtes de colonne appropriées si nécessaire
+if [ ! -f "$OUTPUT_FILE" ] || ! grep -q "timestamp,rate" "$OUTPUT_FILE"; then
+    echo "timestamp,rate" > $OUTPUT_FILE
+fi
 
 # Récupérer le taux de change EUR/USD
-rate=$(curl -s $URL | grep -oP '(?<=<span class="ccOutputTrail">)[^<]+(?=<\/span>)')
+rate=$(curl -s "$URL" | grep -oP '(?<=<span class="ccOutputRslt">)[^<]+(?=<span class="ccOutputTrail">)')
+decimal=$(curl -s "$URL" | grep -oP '(?<=<span class="ccOutputTrail">)[^<]+(?=<\/span>)')
+
+# Concaténer le taux et les décimales
+complete_rate="${rate}${decimal}"
 
 # Afficher le taux de change récupéré
-echo "Taux de change EUR/USD : $rate"
-
+echo "Taux de change EUR/USD : $complete_rate"
 # Enregistrer le taux de change et la date dans data.csv
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$timestamp,$rate" >> $OUTPUT_FILE
+echo "$timestamp,$complete_rate" >> $OUTPUT_FILE
