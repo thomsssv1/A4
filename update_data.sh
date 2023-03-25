8be9891825ab0e5d2ce8b157a5c62375f5115003
thomsssv1 <thomasni.2001@gmail.com>
2023-03-25 16:56:16 +0100

    ca a l'air de marché

--- update_data.sh
+++ update_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-scp -i /home/parallels/Projet_Thomas_A4_Git.pem /home/parallels/A4/data.csv ec2-user@13.48.3.249:/home/parallels/A4/data.csv
+scp -i /home/parallels/Projet_Thomas_A4_Git.pem /home/parallels/A4/data.csv ec2-user@13.48.3.249:/home/ec2-user/A4/
 
 
+scp -i /home/parallels/Projet_Thomas_A4_Git.pem /home/parallels/A4/data.csv ec2-user@13.48.3.249:/home/ec2-user/A4/data.csv
