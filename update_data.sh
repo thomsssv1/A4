434cba219d81917bfed711a8f8e496f3c2cf4d93
thomsssv1 <thomasni.2001@gmail.com>
2023-03-26 11:20:03 +0200

    Mise à jour automatique des données

--- update_data.sh
+++ update_data.sh
@@ -3,4 +3,7 @@
 scp -i /home/parallels/Projet_Thomas_A4_Git.pem /home/parallels/A4/data.csv ec2-user@13.48.3.249:/home/ec2-user/A4/
 
 
-scp -i /home/parallels/Projet_Thomas_A4_Git.pem /home/parallels/A4/data.csv ec2-user@13.48.3.249:/home/ec2-user/A4/data.csv
+cd A4
+git add .
+git commit -m "Mise à jour automatique des données"
+git push
